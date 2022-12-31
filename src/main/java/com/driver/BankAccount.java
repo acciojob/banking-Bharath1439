8e655ad4b01b8ad399ed933148e7d544f87769a9
Bharath <vaddibharath2000@gmail.com>
2022-12-31 15:52:50 +0530

    first commit

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -1,30 +1,88 @@
 package com.driver;
 
+import java.util.Random;
+
 public class BankAccount {
 
     private String name;
     private double balance;
     private double minBalance;
 
-    public BankAccount(String name, double balance, double minBalance) {
 
+
+    public BankAccount(String name, double balance) {
+        this.name=name;
+        this.balance=balance;
     }
 
-    public String generateAccountNumber(int digits, int sum) throws Exception{
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
+
+    public void setMinBalance(double minBalance) {
+        this.minBalance = minBalance;
+    }
+
+    public BankAccount(String name, double balance, double minBalance) {
+        this.name = name;
+        this.balance = balance;
+        this.minBalance = minBalance;
+    }
+
+    public String generateAccountNumber(int digits, int sum) throws Exception {
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
+        Random random = new Random();
 
-        return null;
+        String accountNo = "";
+
+        int totalnumber = 0;
+
+        for (int i = 0; i < digits; i++) {
+
+            int digit = random.nextInt(10);
+            totalnumber = totalnumber + digit;
+            accountNo = accountNo + Integer.toString(digit);
+        }
+        if (totalnumber == sum) {
+            return accountNo;
+        } else {
+            throw new Exception("Account Number can not be generated");
+        }
+
     }
 
     public void deposit(double amount) {
         //add amount to balance
+        this.balance=getBalance()+amount;
 
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
+       try{
+           this.balance=this.balance-amount;
+       }
+       catch(Exception e){
+           System.out.print("balance is less than entered amount");
+       }
 
     }
 
--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -5,24 +5,43 @@ public class SavingsAccount extends BankAccount{
     double maxWithdrawalLimit;
 
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
+        super(name,balance);
         // minimum balance is 0 by default
+        this.maxWithdrawalLimit=maxWithdrawalLimit;
+        this.rate=rate;
 
     }
     public void withdraw(double amount) throws Exception {
         // Might throw the following errors:
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
+        if(amount>this.maxWithdrawalLimit){
+            throw new Exception("Maximum Withdraw Limit Exceed");
+        }
+        if(amount>getBalance()){
+            throw new Exception("Insufficient Balance");
+        }
+        double b=getBalance()-amount;
+        setBalance(b);
 
     }
 
     public double getSimpleInterest(int years){
         // Return the final amount considering that bank gives simple interest on current amount
 
+            double amount = getBalance() * (1 + this.rate * years);
+            return amount;
+
+
+
     }
 
     public double getCompoundInterest(int times, int years){
         // Return the final amount considering that bank gives compound interest on current amount given times per year
-
+        double x=rate/times;
+        double y=years*times;
+        double amount=Math.pow((getBalance()*(1+x)), y);
+        return amount;
     }
 
 }
